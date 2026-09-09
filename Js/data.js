d69d80c1c88d948606060f4b388b2c9fcc0672ca
Z2A2 <z12984926@gmail.com>
2026-09-09 21:09:09 +0100

    Create data.js

---
+++ Js/data.js
@@ -0,0 +1,35 @@
+const events = [
+{
+id:1,
+
+artist:"BTS",
+
+title:"BTS WORLD TOUR 'ARIRANG' IN TORONTO",
+
+date:"SUN • AUG 23, 2026",
+
+time:"8:00 PM",
+
+venue:"Rogers Stadium",
+
+location:"Toronto, ON",
+
+image:"images/bts.jpg",
+
+orderNumber:"29000601112694497",
+
+quantity:1,
+
+section:"N110",
+
+row:"24",
+
+seat:"14",
+
+transfer:true,
+
+sell:false,
+
+upgrade:false
+}
+];
